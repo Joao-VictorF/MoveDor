aaedd62b4c66ef655b73a3a4c1136bb33659af26
anselmo157 <anselmoparente@gmail.com>
2020-12-27 16:48:12 -0300

    Implementado lógica para saber se já concluiu 90% do vídeo

--- lib/screens/book/chapters_content/chapter03.dart
+++ lib/screens/book/chapters_content/chapter03.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:movedor/constants.dart';
@@ -16,60 +18,63 @@ class Chapter03 extends StatefulWidget {
 }
 
 class _Chapter03State extends State<Chapter03> {
-  
+  bool finalizouChapter03 = false;
+
   VideoPlayerController _controller;
 
-  // void initState() {
-  //   super.initState();
-  //   SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
 
-  //   SystemChrome.setPreferredOrientations([
-  //     DeviceOrientation.landscapeLeft,
-  //   ]);
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+    ]);
 
-  //   _controller = VideoPlayerController.asset('assets/videos/cap1.mp4');
+    _controller = VideoPlayerController.asset('assets/videos/cap1.mp4');
 
-  //   _controller = VideoPlayerController.asset(
-  //     'assets/videos/cap1.mp4')
-  //     ..initialize().then((_) {
-  //       // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
-  //       setState(() {});
-  //     });
-  // }
+    _controller = VideoPlayerController.asset('assets/videos/cap1.mp4')
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {
+          var tempo = _controller.value.duration * 0.9;
+          Timer(tempo, () {
+            finalizouChapter03 = true;
+          });
+        });
+      });
+  }
 
-  // @override
-  // void dispose() {
-  //   SystemChrome.setEnabledSystemUIOverlays(SystemUiOverlay.values);
-  //   SystemChrome.setPreferredOrientations([
-  //     DeviceOrientation.portraitUp,
-  //   ]);
-  //   super.dispose();
-  //   _controller.dispose();
-  // }
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIOverlays(SystemUiOverlay.values);
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+    super.dispose();
+    _controller.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFF5F6F9),
       appBar: CustomAppBar(chapters[2]),
-      body:  
-      SingleChildScrollView(
-        child: 
-          Column(
-            children: [
-              SizedBox(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
                 width: 230,
                 height: 230,
                 child: AspectRatio(
                   aspectRatio: 1,
-                  child: Image.asset(chapters[2].image, scale: 0.6,),
-                )
-              ),
-              
-              TopRoundedContainer(
-                color: Colors.white, 
-                child: 
-                Column(
+                  child: Image.asset(
+                    chapters[2].image,
+                    scale: 0.6,
+                  ),
+                )),
+            TopRoundedContainer(
+                color: Colors.white,
+                child: Column(
                   children: [
                     Text(
                       chapters[2].title,
@@ -79,29 +84,29 @@ class _Chapter03State extends State<Chapter03> {
                         fontSize: 25,
                       ),
                     ),
-                    // Container(
-                    //   padding: const EdgeInsets.only(top: 20),
-                    //   child: AspectRatio(
-                    //     aspectRatio: _controller.value.aspectRatio,
-                    //     child: Stack(
-                    //       alignment: Alignment.bottomCenter,
-                    //       children: <Widget>[
-                    //         VideoPlayer(_controller),
-                    //         PlayPauseOverlay(controller: _controller),
-                    //         VideoProgressIndicator(_controller, allowScrubbing: true),
-                    //       ],
-                    //     ),
-                    //   ),
-                    // )
+                    Container(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: AspectRatio(
+                        aspectRatio: _controller.value.aspectRatio,
+                        child: Stack(
+                          alignment: Alignment.bottomCenter,
+                          children: <Widget>[
+                            VideoPlayer(_controller),
+                            PlayPauseOverlay(controller: _controller),
+                            VideoProgressIndicator(_controller,
+                                allowScrubbing: true),
+                          ],
+                        ),
+                      ),
+                    )
                     // ProductDescription(
                     //   product,
                     // ),
                     // ProductPricing(product),
                   ],
-                )
-              )
-            ],
-          ),
+                ))
+          ],
+        ),
       ),
     );
   }
